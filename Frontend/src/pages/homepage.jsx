4b1f955efcbb6df11bea9407fcbd825d0ada929a
Moundirbechikh <moundirbechikh03@gmail.com>
2026-03-03 03:52:25 +0100

    Update Homepage component with new layout
    Refactor Homepage component layout and structure.

--- Frontend/src/pages/homepage.jsx
+++ Frontend/src/pages/homepage.jsx
@@ -7,24 +7,29 @@ import Foot from "../components/Foot";
 import Appmenu from "../components/appmenu";
 import Secretform from "../components/Secretform";
 
-
 const Homepage = () => {
-    return (
-        <>
-        <div className="h-screen bg-center bg-no-repeat bg-cover"
-        style={{ backgroundImage: "url('../back1.png')" }}>
-        <Navbar/>
-        <Citation/>
-        </div>
-        <div className="bg-black">
-        <Rate/>
-        <Appmenu/>
-        <About/>
-        <Secretform/>
-        <Foot/>
-        </div>
-        </>
-    );
-  };
-  
-  export default Homepage;
+  return (
+    <>
+      {/* Section Hero */}
+      <div 
+        className="min-h-screen bg-center bg-no-repeat bg-cover flex flex-col"
+        style={{ backgroundImage: "url('../back1.png')" }}
+      >
+        <Navbar />
+        <Citation />
+      </div>
+
+      {/* Contenu principal */}
+      <div className="bg-black flex flex-col space-y-12">
+          <Appmenu />
+        <Rate />
+        
+        <About />
+        <Secretform />
+        <Foot />
+      </div>
+    </>
+  );
+};
+
+export default Homepage;
