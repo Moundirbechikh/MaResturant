e3bcbd52c372743b6025638beb20efcd1ab2dbc6
Moundirbechikh <moundirbechikh03@gmail.com>
2026-03-03 03:35:59 +0100

    Update API URL to use production endpoint

--- Frontend/src/components/menuorder.jsx
+++ Frontend/src/components/menuorder.jsx
@@ -11,7 +11,7 @@ const Menuorder = ({ titre }) => {
     const fetchMenus = async () => {
       try {
         const categorieUrl = titre.toLowerCase().trim(); // nettoyage du titre
-        const response = await fetch(`http://localhost:5000/api/menu/${categorieUrl}`);
+        const response = await fetch(`https://maresturant.onrender.com/api/menu/${categorieUrl}`);
 
         if (!response.ok) {
           throw new Error(`Erreur HTTP: ${response.status}`);
