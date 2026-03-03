eb6ff4a095ee12600d9997c6a76fddd5c7c6727b
Moundirbechikh <moundirbechikh03@gmail.com>
2026-03-03 03:30:15 +0100

    Change transition duration for top circle in Card component
    Increased the transition duration for the top circle from 500ms to 1500ms.

--- Frontend/src/components/Card.jsx
+++ Frontend/src/components/Card.jsx
@@ -10,7 +10,7 @@ const Card = ({ image, text, onClick }) => {
       <div className="z-10 absolute w-full h-full peer"></div>
 
       {/* Cercle haut */}
-      <div className="absolute peer-hover:-top-20 peer-hover:-left-16 peer-hover:w-[140%] peer-hover:h-[140%] -top-32 -left-16 w-24 h-32 sm:w-32 sm:h-44 rounded-full bg-red-800 transition-all duration-500"></div>
+      <div className="absolute peer-hover:-top-20 peer-hover:-left-16 peer-hover:w-[140%] peer-hover:h-[140%] -top-32 -left-16 w-24 h-32 sm:w-32 sm:h-44 rounded-full bg-red-800 transition-all duration-1500"></div>
 
       {/* Cercle bas avec texte */}
       <div className="absolute flex text-center items-end justify-end peer-hover:right-0 peer-hover:rounded-b-none peer-hover:bottom-0 peer-hover:items-center peer-hover:justify-center peer-hover:w-full peer-hover:h-full -bottom-24 -right-10 w-28 h-36 sm:w-36 sm:h-44 rounded-full bg-red-800 transition-all duration-500 text-white">
